f2e2f2468101edbe7e16d0ce3b909b2d09f7544e
Cian Barreto <cianbluewind@hotmail.com>
2021-03-23 00:02:31 -0300

    Fixed database load

--- my-entrypoint.sh
+++ my-entrypoint.sh
@@ -1,4 +1,4 @@
-echo "Running Backup & Restore"
+echo "Running Backup & Restore"	
 neo4j-admin load --from=/neo4j.dump --database=neo4j --force
 chown -R neo4j:neo4j /data
 /docker-entrypoint.sh neo4j start
